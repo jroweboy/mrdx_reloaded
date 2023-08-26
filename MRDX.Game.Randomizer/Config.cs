26cac46c8a071658005fae3cc87b59f8f01789b2
James Rowe <jroweboy@gmail.com>
2023-08-26 11:03:31 -0400

    More work on swapping moves

--- MRDX.Game.Randomizer/Config.cs
+++ MRDX.Game.Randomizer/Config.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel;
 using MRDX.Game.Randomizer.Template.Configuration;
-using Reloaded.Mod.Interfaces.Structs;
 
 namespace MRDX.Game.Randomizer.Configuration;
 
@@ -32,6 +31,10 @@ public class Config : Configurable<Config>
     public string FlagString { get; set; } = "";
 }
 
+/// <summary>
+///     Allows you to override certain aspects of the configuration creation process (e.g. create multiple configurations).
+///     Override elements in <see cref="ConfiguratorMixinBase" /> for finer control.
+/// </summary>
 public class ConfiguratorMixin : ConfiguratorMixinBase
 {
     // 
--- MRDX.Game.Randomizer/Mod.cs
+++ MRDX.Game.Randomizer/Mod.cs
@@ -1,4 +1,4 @@
-﻿
+
 using MRDX.Base.ExtractDataBin.Interface;
 using MRDX.Game.Randomizer.Configuration;
 using MRDX.Game.Randomizer.Template;
@@ -9,40 +9,40 @@ using Reloaded.Universal.Redirector.Interfaces;
 namespace MRDX.Game.Randomizer;
 
 /// <summary>
-/// Your mod logic goes here.
+///     Your mod logic goes here.
 /// </summary>
 public class Mod : ModBase // <= Do not Remove.
 {
     /// <summary>
-    /// Provides access to the mod loader API.
-    /// </summary>
-    private readonly IModLoader _modLoader;
-
-    /// <summary>
-    /// Provides access to the Reloaded.Hooks API.
+    ///     Provides access to the Reloaded.Hooks API.
     /// </summary>
     /// <remarks>This is null if you remove dependency on Reloaded.SharedLib.Hooks in your mod.</remarks>
     private readonly IReloadedHooks? _hooks;
 
     /// <summary>
-    /// Provides access to the Reloaded logger.
+    ///     Provides access to the Reloaded logger.
     /// </summary>
     private readonly ILogger _logger;
 
     /// <summary>
-    /// Entry point into the mod, instance that created this class.
+    ///     The configuration of the currently executing mod.
+    /// </summary>
+    private readonly IModConfig _modConfig;
+
+    /// <summary>
+    ///     Provides access to the mod loader API.
+    /// </summary>
+    private readonly IModLoader _modLoader;
+
+    /// <summary>
+    ///     Entry point into the mod, instance that created this class.
     /// </summary>
     private readonly IMod _owner;
 
     /// <summary>
-    /// Provides access to this mod's configuration.
+    ///     Provides access to this mod's configuration.
     /// </summary>
     private Config _configuration;
-
-    /// <summary>
-    /// The configuration of the currently executing mod.
-    /// </summary>
-    private readonly IModConfig _modConfig;
 
     public Mod(ModContext context)
     {
@@ -83,6 +83,16 @@ public class Mod : ModBase // <= Do not Remove.
             extract.ExtractComplete += randomizer.DataExtractComplete;
     }
 
+    #region For Exports, Serialization etc.
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public Mod()
+    {
+    }
+#pragma warning restore CS8618
+
+    #endregion
+
     #region Standard Overrides
 
     public override void ConfigurationUpdated(Config configuration)
@@ -92,16 +102,6 @@ public class Mod : ModBase // <= Do not Remove.
         _configuration = configuration;
         _logger.WriteLine($"[{_modConfig.ModId}] Config Updated: Applying");
     }
-
-    #endregion
-
-    #region For Exports, Serialization etc.
-
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-    public Mod()
-    {
-    }
-#pragma warning restore CS8618
 
     #endregion
 }
